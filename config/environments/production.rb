5f0e1201cd6812a86280a83e0fd21fd4ab478047
Remy Phan <phan.remy@gmail.com>
2024-02-17 11:56:00 +0530

    rename Yatai Inventory

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails7_importmaps_tailwind_project_template_production"
+  # config.active_job.queue_name_prefix = "yatai_inventory_production"
 
   config.action_mailer.perform_caching = false
 
